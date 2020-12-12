6a3c2237037fba7985cb76d34c519f92dd641993
AniTecK <aniteck82@hotmail.fr>
2020-12-12 18:43:50 +0100

    Navbar base

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -3,60 +3,120 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faBars, faShoppingBasket, faUserCircle} from '@fortawesome/free-solid-svg-icons';
 import { Transition } from 'react-transition-group';
+import { colors } from "@theme";
 
 const Navbar: React.FC = () => {
 
   const [deployed, setDeployed] = useState(false);
   const transitionStyles = {
-    entering: { transform: "translate3d(0, 100%, 0)" },
+    entering: { transform: "translate3d(12px, 50%, 3em)" },
     entered:  { opacity: 1 },
     exiting:  { opacity: 0 },
     exited:  { opacity: 0 },
   };
 
+  const Fade = ({ in: inProp }) => (
+    <Transition in={inProp} timeout={duration}>
+      {state => (
+        <div style={{
+          ...defaultStyle,
+          ...transitionStyles[state]
+        }}>
+          I'm a fade Transition!
+        </div>
+      )}
+    </Transition>
+  );
+
   return (
-    <MainContainer>
-      <FontAwesomeIcon
-        id={"bars"}
-        icon={faBars}
-        onClick={() => {
-          setDeployed(!deployed)
-        }}
-      />
+    <Wrapper>
 
-      <a href="">
-        <img src="/static/resources/LOGO1.svg" alt="" />
-      </a>
+      <MainContainer>
 
-      <div className={"icons-container"}>
+        <FontAwesomeIcon
+          id={"bars"}
+          icon={faBars}
+          onClick={() => {
+            setDeployed(!deployed)
+          }}
+        />
+
         <a href="">
-          <FontAwesomeIcon icon={faUserCircle}/>
+          <img src="/static/resources/LOGO1.svg" alt="" />
         </a>
-        <a href="">
-          <FontAwesomeIcon icon={faShoppingBasket}/>
-        </a>
-      </div>
 
-    </MainContainer>
+        <div className={"icons-container"}>
+          <a href="">
+            <FontAwesomeIcon icon={faUserCircle}/>
+          </a>
+          <a href="">
+            <FontAwesomeIcon icon={faShoppingBasket}/>
+          </a>
+        </div>
+
+      </MainContainer>
+
+      <Menu className={deployed ? "deployed" : ""}>
+
+        <section>
+          SEARCH BAR
+        </section>
+
+        <section>
+          Client
+        </section>
+
+        <section>
+          Commerçant
+        </section>
+
+      </Menu>
+
+    </Wrapper>
   )
 }
 
+const Wrapper = styled.div `
+  width: 100%;
+  position: sticky;
+  z-index: 2;
+`
+
+const Menu = styled.div `
+  color: ${colors.light};
+  width: 100%;
+  padding: 20px;
+  z-index: -1;
+  background: ${colors.secondary};
+  position: absolute;
+  transform: translate3d(0,-100%,0);
+  transition: 0.7s;
+  padding-bottom: 45px;
+  display: flex;
+  justify-content: space-around;
+
+  &.deployed {
+    transform: translate3d(0,0,0);
+    padding-bottom: 20px;
+  }
+
+  section {
+    margin: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
 const MainContainer = styled.div `
-  color: #e18380;
+  color: ${colors.primary};
   font-size: 25px;
   height: 100px;
   width: 100%;
-  background: #d7e8e1;
+  background: ${colors.light};
   display: flex;
   justify-content: space-around;
   align-items: center;
-  z-index: 2;
-
-  .navbar-container {
-    width: 100%;
-    padding: 20px;
-    z-index: 1;
-  }
 
   img {
     width: 150px;
@@ -64,7 +124,8 @@ const MainContainer = styled.div `
   }
 
   #bars {
-    color: #209d83;
+    color: ${colors.secondary};
+    cursor: pointer;
   }
 
   .icons-container svg {
---
+++ src/theme/index.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  primary: "#e18380",
+  secondary: "#209d83",
+  light: "#d7e8e1"
+}
