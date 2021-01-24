25d3366f41ef0d5325671a6a5a23ff0487bcd44a
Octanium <octaniumcontact@gmail.com>
2021-01-24 15:42:59 +0100

    patch display font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-      display: ['Teko', ...defaultTheme.fontFamily.display],
+      display: ['Teko'],
     },
     colors: {
       'darker-primary': 'var(--color-darker-primary)',
