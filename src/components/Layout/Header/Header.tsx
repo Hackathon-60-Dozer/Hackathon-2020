c8725b7bacd6fef04b78c10e9fe6d3248cd9e234
AniTecK <aniteck82@hotmail.fr>
2021-01-24 16:38:05 +0100

    header mobile version

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Header: React.FC = () => {
-  return <header></header>;
+  return (
+    <header className="flex justify-between p-3 items-baseline shadow z-10">
+      <button className="flex justify-center items-center">
+        <GiHamburgerMenu />
+      </button>
+      <h2 className="font-display text-2xl">l'Echoppe Toulousaine</h2>
+      <button className="rounded-full bg-secondary text-primary flex items-center justify-center w-8 h-8">
+        <FiShoppingCart />
+      </button>
+    </header>
+  );
 };
 
 export default Header;
